240b9968abf25418ff8e4330d4eb0e232e8e4d0e
desperus <aleksander.meterko@gmail.com>
2020-04-23 08:54:36 -0400

    Added simple test for KafkaConsumer

--- src/main/kotlin/com/epam/shopping/kafka/ProductChangeEventConsumer.kt
+++ src/main/kotlin/com/epam/shopping/kafka/ProductChangeEventConsumer.kt
@@ -1,12 +1,18 @@
 package com.epam.shopping.kafka
 
+import com.epam.shopping.model.ProductChangeEvent
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 
 class ProductChangeEventConsumer {
 
-    @KafkaListener(topics = ["#{pocapp.topics}"])
-    fun consumeProductEvents() {
-
+    @KafkaListener(topics = ["\${pocapp.topic.product}"])
+    fun consumeProductEvents(productRecords: List<ConsumerRecord<String, ProductChangeEvent>>) {
+        try {
+            productRecords.forEach { println("Received event [key=${it.key()}, value=${it.value()}") }
+        } catch (e: Exception) {
+            println("Handle failover scenario")
+        }
     }
 
 }
--- src/test/kotlin/com/epam/shopping/ShoppingApplicationTests.kt
+++ src/test/kotlin/com/epam/shopping/ShoppingApplicationTests.kt
@@ -1,13 +1,38 @@
 package com.epam.shopping
 
+import com.epam.shopping.model.ProductChangeEvent
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.test.context.ActiveProfiles
+import java.util.concurrent.TimeUnit
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ShoppingApplicationTests {
+
+    @Autowired
+    private lateinit var kafkaTemplate: KafkaTemplate<String, ProductChangeEvent>
+
+    @Value("\${pocapp.topic.product}")
+    private lateinit var productTopic: String
 
     @Test
     fun contextLoads() {
+    }
+
+    @Test
+    fun `when multiple change events sent they are printed to console`() {
+        // given
+        val productChangeEvent = ProductChangeEvent(1L, "Coffee", 9.0)
+
+        // when
+        val future = kafkaTemplate.send(productTopic, "someKey", productChangeEvent)
+        future.get(10, TimeUnit.SECONDS)
+
+        // then
     }
 
 }
