33afb2830bfc71a8f74dc98f7a8bcbc1735538b8
desperus <aleksander.meterko@gmail.com>
2020-04-27 08:36:39 -0400

    Fixed Gradle build, tests are green

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,30 +11,38 @@ repositories {
     mavenCentral()
 }
 
+object Versions {
+    const val logbackJson = "0.1.5"
+    const val springKafka = "2.3.7.RELEASE"
+}
+
 dependencies {
-    implementation "org.springframework.boot:spring-boot-starter-web:2.2.6.RELEASE"
-    implementation "com.fasterxml.jackson.module:jackson-module-kotlin:2.10.3"
-    implementation "org.springframework.boot:spring-boot-starter:2.2.6.RELEASE"
-    implementation "org.jetbrains.kotlin:kotlin-reflect:1.3.71"
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.71"
-    implementation "org.springframework.kafka:spring-kafka:2.3.7.RELEASE"
-    implementation "org.slf4j:slf4j-api:1.7.30"
-    implementation "ch.qos.logback.contrib:logback-json-classic:0.1.5"
-    implementation "ch.qos.logback.contrib:logback-jackson:0.1.5"
-    implementation "org.springframework.boot:spring-boot-starter-webflux:2.2.6.RELEASE"
-    testImplementation "org.springframework.boot:spring-boot-starter-test:2.2.6.RELEASE"
-    testImplementation "org.springframework.kafka:spring-kafka-test:2.3.7.RELEASE"
-    testImplementation "org.awaitility:awaitility:3.0.0"
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.3")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.kafka:spring-kafka:${Versions.springKafka}")
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("ch.qos.logback.contrib:logback-json-classic:${Versions.logbackJson}")
+    implementation("ch.qos.logback.contrib:logback-jackson:${Versions.logbackJson}")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation("org.springframework.kafka:spring-kafka-test:${Versions.springKafka}")
+    testImplementation("org.awaitility:awaitility:3.0.0")
 }
 
 group = "com.epam"
 version = "0.0.1-SNAPSHOT"
 description = "shopping"
-sourceCompatibility = "1.8"
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
